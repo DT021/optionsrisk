3d91bdb29f2ef78d6a0413be3fc8e344076000ab
sultrey <sulrey@Andrews-MacBook-Pro-4.local>
2019-04-18 21:28:55 -0700

    I added the bottom section of this website. I added the table wher the option risk results will be inputted. I also added the 'download as CSV' button. For now, the table is filled with names and countries as placeholders

---
+++ riskcalc.js
@@ -0,0 +1,14 @@
+/*
+
+var ticker = document.getElementById('ticker');
+var price = document.getElementById('price');
+var beup = document.getElementById('beup');
+var bedown = document.getElementById('bedown');
+var button = document.getElementById('');
+
+button.addEventListener('click', function(event) {
+						
+						
+});
+
+*/
