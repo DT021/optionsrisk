f3ae13be6ee751bb34a9685c99455460b850b4c1
sultrey <sulrey@Andrews-MacBook-Pro-4.local>
2019-04-19 12:38:00 -0700

    I added the 'download as CSV' function. Now, when the button is pressed, the table with all of the results will download onto your computer as a CSV file which you can open with Excel, Google Sheets, etc. Pretty much done with the project at this point

--- riskcalc.js
+++ riskcalc.js
@@ -1,14 +1,12 @@
-
-
 var ticker = document.getElementById('ticker');
 var price = document.getElementById('price');
 var beup = document.getElementById('beup');
 var bedown = document.getElementById('bedown');
 var calcbutt = document.getElementById('calcbutt');
 var myTable = document.getElementById('myTable'); 
-	
-calcbutt.addEventListener('click', function(event) {
-		
+
+
+function listmaker() {
 	if ((!ticker.value || !ticker)||(!price.value || !price)||(!beup.value || !beup)||(!bedown.value ||!bedown)) {
 		alert("Please enter a value in all of the fields!")
 	} else {
@@ -32,11 +30,60 @@ calcbutt.addEventListener('click', function(event) {
 		var cell3 = row.insertCell(2);
 		
 		cell1.innerText = ticker.value;
-		cell2.innerText = pospercent;
-		cell3.innerText = negpercent;
+		cell2.innerText = pospercent + "%";
+		cell3.innerText = negpercent + "%";
 		
 		event.preventDefault();
 		
 	}
-						
+}
+
+function download_csv(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+}
+
+function export_table_to_csv(html, filename) {
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+	
+    for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+		
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+		csv.push(row.join(","));		
+	}
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+}
+
+document.querySelector("button.yeet").addEventListener("click", function () {
+    var html = document.querySelector("table").outerHTML;
+	export_table_to_csv(html, "options.csv");
 });
+
+
